88cf3b51fc82fc5d5ba6ab2abe8e6e40caf4af4b
nupurkarim88 <nupurkarim88@gmail.com>
2025-01-04 15:57:37 +0600

    Brief on Javascript

---
+++ Introduction to HTML DOM/script2.js
@@ -0,0 +1,24 @@
+function valdate(e) {
+e.PreventDefault();
+
+const email = document.getElementById( 'email').value;
+    const pass = document.getElementById('password').value;
+    const age = document.getElementById('age').value;
+    const msgBox = document.getElementById('message').value;
+    let message = '';
+    if (email === '') {
+        message = ' Enter an email.';
+        msgBox.style.color = 'red';
+    } else if (pass === '') {
+        message = 'Enter a password';
+        msgBox.style.color = 'red'
+    } else if (age === '') {
+        message = 'Enter your age';
+        msgBox.style.color = 'red'
+} else {
+    message = 'Login successfully!';
+    msgBox.style.color = 'green';
+}
+
+msgBox.innerText = message;
+}
